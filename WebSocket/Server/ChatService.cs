24320aaf2f334be87b739ba7cb17f4ec4d7f6ea0
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2025-06-15 11:36:18 +0900

    사용자 연결/종료/메시지 전송 샘플

--- WebSocket/Server/ChatService.cs
+++ WebSocket/Server/ChatService.cs
@@ -1,6 +1,102 @@
-﻿namespace Server
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Server
 {
     public class ChatService
     {
+        public ChatService()
+        {
+
+        }
+
+        public async Task ProcessAsync(HttpContext httpCtx)
+        {
+            if (!httpCtx.WebSockets.IsWebSocketRequest)
+            {
+                httpCtx.Response.StatusCode = 400;
+                return;
+            }
+
+            // userId 읽기
+            var userIdStr = httpCtx.Request.Query["userId"].FirstOrDefault();
+            if (string.IsNullOrEmpty(userIdStr))
+            {
+                httpCtx.Response.StatusCode = 400;
+                return;
+            }
+
+            var userId = ulong.Parse(userIdStr);
+            Console.WriteLine($"[유저 ID]: {userId}");
+
+            // 최초 연결
+            if (_userIdWebSocketDict.ContainsKey(userId))
+            {
+                Console.WriteLine($"[유저 ID]: {userId} 이미 연결됨.");
+                httpCtx.Response.StatusCode = 400;
+                return;
+            }
+
+            var socket = await httpCtx.WebSockets.AcceptWebSocketAsync();
+            if (!_userIdWebSocketDict.TryAdd(userId, socket))
+            {
+                Console.WriteLine($"[유저 ID]: {userId} 이미 연결됨 ?????.");
+                httpCtx.Response.StatusCode = 400;
+                return;
+            }
+
+            var buffer = new byte[1024 * 4];
+            var messageBuffer = new MemoryStream();
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                switch (result.MessageType)
+                {
+                    case WebSocketMessageType.Text:
+                        {
+                            // 이번에 받은 부분만 출력
+                            var partialMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                            Console.WriteLine($"[수신 조각]: {partialMessage}");
+
+                            messageBuffer.Write(buffer, 0, result.Count);
+
+                            if (result.EndOfMessage)
+                            {
+                                var messageBytes = messageBuffer.ToArray();
+                                var message = Encoding.UTF8.GetString(messageBytes);
+
+                                var sendMessage = $"[Echo] {message}";
+                                Console.WriteLine(sendMessage);
+                                var sendBuffer = Encoding.UTF8.GetBytes(sendMessage);
+                                foreach (var ws in _userIdWebSocketDict.Values.Where(w => w.State == WebSocketState.Open))
+                                {
+                                    await ws.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                                }
+                            }
+                        }
+                        break;
+                    case WebSocketMessageType.Binary:
+                        break;
+                    case WebSocketMessageType.Close:
+                        {
+                            if (!_userIdWebSocketDict.TryRemove(userId, out var connectedWebSocket))
+                            {
+                                Console.WriteLine($"클라이언트 연결 종료 실패 : {userId}");
+                                httpCtx.Response.StatusCode = 400;
+                                return;
+                            }
+
+                            await connectedWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by client", CancellationToken.None);
+                            Console.WriteLine($"클라이언트 연결 종료 : {userId}");
+                        }
+                        break;
+                }
+            }
+        }
+
+        private ConcurrentDictionary<ulong, WebSocket> _userIdWebSocketDict = new ConcurrentDictionary<ulong, WebSocket>();
     }
 }
--- WebSocket/Server/Program.cs
+++ WebSocket/Server/Program.cs
@@ -1,7 +1,10 @@
+癤퓎sing Server;
 using System.Net.WebSockets;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ChatService>();
+
 var app = builder.Build();
 
 var webSocketConnections = new List<WebSocket>();
@@ -11,41 +14,8 @@ app.MapGet("/", () => "Hello World!");
 
 app.Map("/ws", async context =>
 {
-    if (context.WebSockets.IsWebSocketRequest)
-    {
-        var socket = await context.WebSockets.AcceptWebSocketAsync();
-        webSocketConnections.Add(socket);
-
-        var buffer = new byte[1024 * 4];
-
-        while (socket.State == WebSocketState.Open)
-        {
-            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-            if (result.MessageType == WebSocketMessageType.Text)
-            {
-                var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                Console.WriteLine($"[수신]: {message}");
-
-                var sendBuffer = Encoding.UTF8.GetBytes($"[Echo] {message}");
-
-                foreach (var ws in webSocketConnections.Where(w => w.State == WebSocketState.Open))
-                {
-                    await ws.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
-                }
-            }
-            else if (result.MessageType == WebSocketMessageType.Close)
-            {
-                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by client", CancellationToken.None);
-                webSocketConnections.Remove(socket);
-                Console.WriteLine("클라이언트 연결 종료");
-            }
-        }
-    }
-    else
-    {
-        context.Response.StatusCode = 400;
-    }
+    var charSvc = context.RequestServices.GetRequiredService<ChatService>();
+    await charSvc.ProcessAsync(context); 
 });
 
 app.Run();
